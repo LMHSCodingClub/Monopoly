435fb0ae0842abe75f8ed71f6b84be0c1c76cf29
Varun <31084854+Borumer@users.noreply.github.com>
2021-05-11 16:21:50 -0400

    Fix offsets, make drawProperties() more readable * Implement createPropertyTile to create light blue fill and black border (requires two separate rectangles) * Remove monopoly submodule (not needed)

--- src/main/java/com/lmhscodingclub/monopoly/userinterface/Board.java
+++ src/main/java/com/lmhscodingclub/monopoly/userinterface/Board.java
@@ -1,49 +1,48 @@
 package com.lmhscodingclub.monopoly.userinterface;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.swing.JPanel;
-import javax.swing.ImageIcon;
-import javax.swing.BorderFactory;
-
-import java.awt.Graphics2D;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Dimension;
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.AffineTransform;
-
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
 
 public class Board extends JPanel {
-    private Color green = new Color(0, 255, 0);
-    private Color white = new Color(255, 255, 255);
     private int width = 500;
     private int height = 500;
-    
+    private Color boardColor = new Color(0, 125, 255);
+
     public Board() {
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(250, 200);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(green);
-        g2.setBackground(green);
-        
+        g2.setColor(Color.GREEN);
+        g2.setBackground(Color.GREEN);
+
         // Outer and inner borders
         g2.fill(new Rectangle2D.Double(62.5, 62.5, width, height));
-        
+
         try {
             drawInnerRectangles(g2);
             drawProperties(g2);
+            drawMonopolyLogo(g2);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -53,8 +52,8 @@ public class Board extends JPanel {
     private void drawInnerRectangles(Graphics2D g2) throws IOException {
         // Rectangles on inside of board that are rotated 45 degrees
         AffineTransform old = g2.getTransform();
-        
-        //g2.rotate(Math.toRadians(-45));
+
+        // g2.rotate(Math.toRadians(-45));
 
         // Chance
         g2.setColor(new Color(255, 165, 0));
@@ -68,7 +67,7 @@ public class Board extends JPanel {
         g2.fill(new Rectangle2D.Double(400, 400, 100, 50));
 
         // Monopoly logo
-        //  drawMonopolyLogo(g2);
+        // drawMonopolyLogo(g2);
 
         g2.setTransform(old);
     }
@@ -82,28 +81,42 @@ public class Board extends JPanel {
     }
 
     private void drawProperties(Graphics2D g2) {
-        g2.setColor(white);
-        g2.setBackground(green);
-
         // Properties
         // x, y, w, h
-        //Dimensions of tile: length - 62.4, width - 41.6, corner tiles - 62.4 X 62.4
-        
-        // top row
-        for(int i = 0; i < height; i+=41.6){
-            g2.fill(new Rectangle2D.Double(62.4+i, 62.4, 41.6, 62.4));
+
+        final float TILE_WIDTH = 41.6f;
+        final float TILE_HEIGHT = 62.4f;
+        final float TILE_OFFSET_X = 41.6f;
+        final float TILE_OFFSET_Y = 499.1f;
+
+        g2.setColor(Color.BLACK);
+        Paint blackBorderWhiteFill = new GradientPaint(0, 0, Color.WHITE, TILE_WIDTH, TILE_HEIGHT, Color.BLACK);
+        g2.setPaint(blackBorderWhiteFill);
+        g2.setBackground(Color.GREEN);
+
+        // Dimensions of tile: length - TILE_WIDTH, width - 41.6, corner tiles -
+        // TILE_WIDTH X TILE_WIDTH
+
+        // Top and bottom rows
+        for (int i = 0; i < 10; i++) {
+            // Top
+            createPropertyTile(g2, i * TILE_HEIGHT + TILE_OFFSET_X, TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+
+            // Bottom
+            createPropertyTile(g2, i * TILE_HEIGHT + TILE_OFFSET_X, TILE_OFFSET_Y, TILE_WIDTH, TILE_HEIGHT);
         }
-        // bottom row
-        for(int i = 0; i < height; i+=41.6){
-            g2.fill(new Rectangle2D.Double(62.4+i, 499.1, 41.6, 62.4));
+
+        // Left and right columns
+        for (int q = 1; q <= 8; q++) {
+            createPropertyTile(g2, TILE_OFFSET_X + 20, TILE_HEIGHT * q, TILE_HEIGHT + TILE_OFFSET_X, TILE_OFFSET_X);
+            createPropertyTile(g2, TILE_OFFSET_X + 520, TILE_HEIGHT * q, TILE_HEIGHT + TILE_OFFSET_X, TILE_OFFSET_X);
         }
-        // left side
-        for(int q = 0; q < height; q+=41.6){
-            g2.fill(new Rectangle2D.Double(0, 62.4+q, 62.4, 41.6));
-        }
-        // right side
-        for(int q = 0; q < height; q+=41.6){
-            g2.fill(new Rectangle2D.Double(550, 62.4+q, 62.4, 41.6));
-        }
+    }
+
+    private void createPropertyTile(Graphics2D g2, float x, float y, float w, float h) {
+        g2.setPaint(boardColor);
+        g2.fill(new Rectangle2D.Double(x, y, w, h));
+        g2.setPaint(Color.BLACK);
+        g2.draw(new Rectangle2D.Double(x, y, w, h));
     }
 }
